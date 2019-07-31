7b03da45917aba815a879d7450c48367d6352476
wangyimei <nina.wangyimei@gmail.com>
2019-07-31 10:30:36 +0800

    Updates

---
+++ precache-manifest.8c077c011eea9210897a802c84b4fe7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea86d26fcebb589fdea87e3b898a477",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "2b5be76117da2e6ed8cd",
+    "url": "/react-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-website/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2b5be76117da2e6ed8cd",
+    "url": "/react-website/static/js/main.6b9ac51f.chunk.js"
+  },
+  {
+    "revision": "59b994fe6cb40dbf3519",
+    "url": "/react-website/static/js/runtime~main.5bd11a58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
